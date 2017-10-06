3ece3284c0f42c437dfb579a66581175d6ec7672
mdss@makexhtml.ru <mdss@makexhtml.ru>
2017-10-06 12:20:44 +0300

    homepage

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+(function($){ 
+	"use strict";
+
+	$('.change-system .change-system__row').click(function() {
+		$(this).addClass('change-system__row--active').siblings().removeClass('change-system__row--active');
+		changePutValue()
+	});
+	changePutValue()
+
+	function changePutValue() {
+		var changeGivePic = $('.change-system--give .change-system__row--active').find('.change-system__icon').attr('src'),
+		changeGetPic = $('.change-system--get .change-system__row--active').find('.change-system__icon').attr('src'),
+		changeGiveName = $('.change-system--give .change-system__row--active').find('.change-system__name').text(),
+		changeGetName = $('.change-system--get .change-system__row--active').find('.change-system__name').text();
+		$('.change__section-give .change__section-pic').attr('src', changeGivePic);
+		$('.change__section-get .change__section-pic').attr('src', changeGetPic);
+		$('.change__section-give .change__section-input').val(changeGiveName);
+		$('.change__section-get .change__section-input').val(changeGetName);
+	}
+
+	console.log('%c Верстка: mdss@makexhtml.ru ', 'color:#fff;font-size:1.2rem;background-color:#3469c6;')
+
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,134 @@
+'use strict';
+
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	uglify = require('gulp-uglify'),
+	stylus = require('gulp-stylus'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rigger = require('gulp-rigger'),
+	cssMin = require('gulp-clean-css'),
+	concat = require('gulp-concat'),
+	rimraf = require('rimraf'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
+
+var path = {
+	build: {
+		html: 'dist/',
+		js: 'dist/js/',
+		css: 'dist/css/',
+		img: 'dist/i/',
+		fonts: 'dist/fonts/'
+	},
+	src: {
+		html: 'src/*.html',
+		js: 'src/js/*.js',
+		css: 'src/style/*.styl',
+		img: 'src/i/**',
+		fonts: 'src/fonts/**',
+		cssLibs: 'src/style/libs/*.css',
+		jsLibs: 'src/js/libs/*.js'
+	},
+	watch: {
+		html: 'src/**/*.html',
+		js: 'src/js/**/*.js',
+		css: 'src/style/**/*.styl',
+		img: 'src/i/**',
+		fonts: 'src/fonts/**',
+		cssLibs: 'src/style/libs/**/*.css',
+		jsLibs: 'src/js/libs/**/*.js'
+	},
+	clean: './dist'
+};
+gulp.task('webserver', function() {
+	browserSync({
+		server: {
+			baseDir: './build'
+		},
+		host: 'localhost',
+		port: 3000,
+		tunnel: true
+	});
+});
+
+gulp.task('html:build', function() {
+	gulp.src(path.src.html)
+		.pipe(rigger())
+		.pipe(gulp.dest(path.build.html))
+		.pipe(reload({stream: true}));
+});
+gulp.task('js:build', function() {
+	gulp.src(path.src.js)
+		.pipe(gulp.dest(path.build.js))
+		.pipe(reload({stream: true}));
+});
+gulp.task('style:build', function() {
+	gulp.src(path.src.css)
+		.pipe(sourcemaps.init())
+		.pipe(stylus())
+		.pipe(cssMin({compatibility: 'ie8'}))
+		.pipe(concat('main.css'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.build.css))
+		.pipe(reload({stream: true}));
+});
+gulp.task('cssLibs:build', function() {
+	gulp.src(path.src.cssLibs)
+		.pipe(sourcemaps.init())
+		.pipe(concat('plugins.css'))
+		.pipe(cssMin({compatibility: 'ie8'}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.build.css));
+});
+gulp.task('jsLibs:build', function() {
+	gulp.src(path.src.jsLibs)
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(concat('plugins.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.build.js));
+});
+gulp.task('fonts:build', function() {
+	gulp.src(path.src.fonts)
+		.pipe(gulp.dest(path.build.fonts));
+});
+gulp.task('img:build', function() {
+	gulp.src(path.src.img)
+		.pipe(gulp.dest(path.build.img));
+});
+gulp.task('build', [
+	'html:build',
+	'js:build',
+	'style:build',
+	'cssLibs:build',
+	'jsLibs:build',
+	'img:build',
+	'fonts:build'
+]);
+gulp.task('watch', function() {
+	watch([path.watch.js], function(ev, callback) {
+		gulp.start('js:build');
+	});
+	watch([path.watch.html], function(ev, callback) {
+		gulp.start('html:build');
+	});
+	watch([path.watch.css], function(ev, callback) {
+		gulp.start('style:build');
+	});
+	watch([path.watch.img], function(ev, callback) {
+		gulp.start('img:build');
+	});
+	watch([path.watch.fonts], function(ev, callback) {
+		gulp.start('fonts:build');
+	});
+	watch([path.watch.fonts], function(ev, callback) {
+		gulp.start('cssLibs:build');
+	});
+	watch([path.watch.fonts], function(ev, callback) {
+		gulp.start('jsLibs:build');
+	});
+});
+gulp.task('clean', function(callback) {
+	rimraf(path.clean, callback);
+});
+gulp.task('default', ['build', 'webserver', 'watch']);
